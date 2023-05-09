a12ffb7db00f4b07055284d81ed013a1c3f901c1
Marko Gajic <marko.gajic@developion.com>
2023-05-09 17:28:44 +0000

    removed deprecated method

--- src/Web/Twig/Extension.php
+++ src/Web/Twig/Extension.php
@@ -2,8 +2,7 @@
 
 namespace MarcusGaius\Utilities\Web\Twig;
 
-use Craft;
-use craft\web\View;
+use craft\helpers\Cp;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -12,17 +11,12 @@ class Extension extends AbstractExtension
 	public function getFunctions(): array
 	{
 		return [
-			new TwigFunction('field', [$this, 'renderFormMacro'], ['is_safe' => ['html']]),
+			new TwigFunction('field', [$this, 'renderCpFieldHtml'], ['is_safe' => ['html']]),
 		];
 	}
 
-	public function renderFormMacro(string $fieldType, array $fieldOptions): string
+	public function renderCpFieldHtml(string $fieldType, array $fieldOptions = []): string
 	{
-		$oldMode = Craft::$app->view->getTemplateMode();
-		Craft::$app->view->setTemplateMode(View::TEMPLATE_MODE_CP);
-		$html = Craft::$app->view->renderTemplateMacro('_includes/forms', $fieldType, [$fieldOptions]);
-		Craft::$app->view->setTemplateMode($oldMode);
-
-		return $html;
+		return Cp::fieldHtml('template:_includes/forms/' . $fieldType, $fieldOptions);
 	}
 }
